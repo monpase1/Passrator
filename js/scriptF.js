a04c020cf901c3f46f751c4d4103eec8267633df
Maksim <monpase12@gmail.com>
2023-05-31 17:53:04 +0700

    слишком много поменял, уже ничего не помню, но пытаюсь всё сделать

--- js/scriptF.js
+++ js/scriptF.js
@@ -1,43 +1,52 @@
 "use strict";
 
-window.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("DOMContentLoaded", () => {
   const adress = document.getElementById("adress"),
     login = document.getElementById("login"),
     generate = document.getElementById("generate"),
     gutPass = document.getElementById("password"),
     reset = document.getElementById("reset"),
     good = document.getElementById("good"),
-    body = document.querySelector("body");
+    body = document.querySelector("body"),
+    forms = document.querySelectorAll("form");
 
   let counter = 0,
-    password,
-    user;
+    password;
 
-  class User {
-    constructor(id, adress, login, password) {
-      this.id = id;
-      this.adress = adress;
-      this.login = login;
-      this.password = password;
-    }
-  }
-  function generatePassword() {
-    return (
-      Math.random().toString(36).substring(2, 15) +
-      Math.random().toString(36).substring(2, 15)
-    );
-  }
+  const generatePassword = () =>
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15);
+
   function showOptions() {
     good.classList.toggle("show");
     reset.classList.toggle("show");
   }
+  function postData(form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      console.dir(e);
 
+      const req = new XMLHttpRequest();
+      req.open("POST", "server.php", true);
+      const formData = new FormData(form);
+      req.send(formData);
+      req.addEventListener("load", () => {
+        if (req.status === 200) {
+          console.log(req.response);
+        }
+      });
+    });
+  }
+
+  forms.forEach((form) => postData(form));
   generate.addEventListener("click", () => {
-    if (
-      adress.value !== "" ||
-      adress.value !== "https://" ||
-      login.value === ""
-    ) {
+    if (adress.value != "" && adress.value != "https://" && login.value != "") {
+      const info = {
+        adress: adress.value,
+        login: login.value,
+      };
+      adress.classList.add("hide");
+      login.classList.add("hide");
       password = generatePassword();
       gutPass.innerHTML = password;
       showOptions();
@@ -45,29 +54,19 @@ window.addEventListener("DOMContentLoaded", function () {
     adress.value = "";
     login.value = "";
   });
-
   reset.addEventListener("click", () => {
     password = generatePassword();
     gutPass.innerHTML = password;
   });
-  // body.addEventListener("keypress", (e) => {
-  //   console.log(e);
-  //   if (e.key === "r") {
-  //     location.reload();
-  //   }
-  // });
   body.addEventListener("keydown", (e) => {
-    console.log(e);
     if (e.key === "r") {
       location.reload();
     }
   });
 
-  good.addEventListener("click", () => {
-    counter++;
-    user = new User(counter, adress.value, login.value, password);
-    adress.value = "https://";
-    login.value = "monpase1";
+  good.addEventListener("click", (e) => {
+    e.preventDefault();
+
     showOptions();
   });
 });
